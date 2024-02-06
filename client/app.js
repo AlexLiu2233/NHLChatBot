fb3af8563ce0a9981b59fb163dd2d84ad8f20484
AlexLiu2233 <alexliu4043@gmail.com>
2024-02-05 18:16:36 -0800

    Finished up to Task6

--- client/app.js
+++ client/app.js
@@ -20,7 +20,7 @@ var messageBox = createDOM(
 
 // Define the LobbyView class
 class LobbyView {
-  constructor() {
+  constructor(lobby) {
     this.elem = createDOM(
       '<div class="view" id="app-view">' +
       '<ul class="menu" id="app-menu">' +
@@ -44,8 +44,53 @@ class LobbyView {
       '</div>' +
       '</div>'
     );
+
+    // Store references to specific DOM elements
+    this.lobby = lobby; // Assigning the received lobby argument to the property
+    this.listElem = this.elem.querySelector('.room-list'); // ul.room-list element
+    this.inputElem = this.elem.querySelector('.page-control-input'); // input element for new room name
+    this.buttonElem = this.elem.querySelector('.page-control-button'); // button for creating a room
+
+    this.redrawList(); // Call redrawList to draw the initial list of rooms
+
+    this.buttonElem.addEventListener('click', () => {
+      const roomName = this.inputElem.value;
+      if(roomName) {
+        this.lobby.addRoom(Date.now().toString(), roomName); // Using current timestamp as unique ID
+        this.redrawList();
+        this.inputElem.value = ''; // Clear the input field
+      }
+    });
+  }
+
+  // Code in part generated
+  redrawList() {
+    // First, clear the list to ensure it's empty before adding updated room list
+    this.listElem.innerHTML = ''; 
+  
+    // Loop through each room object in the lobby's rooms
+    Object.values(this.lobby.rooms).forEach(room => {
+      // Create a new list item element for each room
+      const roomElement = document.createElement('li');
+      roomElement.className = 'room'; // Assign class for styling
+  
+      // Create a hyperlink for each room that leads to its chat page
+      const roomLink = document.createElement('a');
+      roomLink.className = 'room-link'; // Assign class for styling
+      roomLink.href = `#/chat/${room.id}`; // Dynamic href that includes the room's ID
+  
+      // Set the inner HTML of the link to display the room's image and name
+      roomLink.innerHTML = `<img class="chat-icon" src="${room.image}"> ${room.name}`;
+  
+      // Append the link to the list item
+      roomElement.appendChild(roomLink);
+  
+      // Finally, append the list item to the list element in the DOM
+      this.listElem.appendChild(roomElement);
+    });
   }
 }
+  
 
 // Define the ChatView class
 class ChatView {
@@ -78,8 +123,14 @@ class ChatView {
           </div>
       </div>
   </div>
-  `
+  `  
     );
+
+    this.titleElem = this.elem.querySelector('.room-name'); // h4 element for the room name
+    this.chatElem = this.elem.querySelector('.message-list'); // div.message-list container
+    this.inputElem = this.elem.querySelector('textarea'); // textarea for entering message
+    this.buttonElem = this.elem.querySelector('button'); // button for sending message
+
   }
 }
 
@@ -110,12 +161,52 @@ class ProfileView {
   }
 }
 
+// Define the Room class
+class Room {
+  constructor(id, name, image = 'assets/everyone-icon.png', messages = []) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.messages = messages;
+  }
+
+  addMessage(username, text) {
+    if (!text.trim()) return; // Ignore empty or whitespace-only messages
+    const message = { username, text };
+    this.messages.push(message);
+  }
+}
+
+// Define the Lobby class
+class Lobby {
+  constructor() {
+    // Initialize rooms with 4 Room objects
+    this.rooms = {
+      '1': new Room('1', 'Room 1', 'assets/image1.png'),
+      '2': new Room('2', 'Room 2', 'assets/image2.png'),
+      '3': new Room('3', 'Room 3', 'assets/image3.png'),
+      '4': new Room('4', 'Room 4', 'assets/image4.png'),
+    };
+  }
+
+  getRoom(roomId) {
+    return this.rooms[roomId]; // Return the room by id
+  }
+
+  addRoom(id, name, image = 'assets/everyone-icon.png', messages = []) {
+    const newRoom = new Room(id, name, image, messages);
+    this.rooms[id] = newRoom; // Add the new Room object to the rooms
+  }
+}
+
+
 
 // Define the main function that will be called once the page is loaded
 function main() {
 
   // Instantiate view objects
-  const lobbyView = new LobbyView();
+  const lobby = new Lobby(); //new lobby object
+  const lobbyView = new LobbyView(lobby);
   const chatView = new ChatView();
   const profileView = new ProfileView();
 
@@ -144,7 +235,7 @@ function main() {
 
   // Call renderRoute once on initial load
   renderRoute();
-  cpen322.export(arguments.callee, { renderRoute, lobbyView, chatView, profileView });
+  cpen322.export(arguments.callee, { renderRoute, lobbyView, chatView, profileView, lobby });
 }
 
 // Add the main function as the event handler for the window's load event
