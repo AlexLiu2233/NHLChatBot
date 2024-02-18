74583a6a678dfedde926f7dd00a8ecfedb17d19a
AlexLiu2233 <alexliu4043@gmail.com>
2024-02-17 18:59:15 -0800

    Task 2 Passed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const cpen322 = require('./cpen322-tester.js');
 
-function logRequest(req, res, next){
+function logRequest(req, res, next) {
 	console.log(`${new Date()}  ${req.ip} : ${req.method} ${req.path}`);
 	next();
 }
@@ -12,6 +12,18 @@ function logRequest(req, res, next){
 const host = 'localhost';
 const port = 3000;
 const clientApp = path.join(__dirname, 'client');
+
+let chatrooms = [
+	{ id: "room1", name: "Chat Room 1", image: "image1.png" },
+	{ id: "room2", name: "Chat Room 2", image: "image2.png" }
+	// add as many rooms as needed
+];
+
+let messages = {};
+chatrooms.forEach(room => {
+	messages[room.id] = []; // initialize an empty array for each room
+});
+
 
 // express app
 let app = express();
@@ -26,5 +38,18 @@ app.listen(port, () => {
 	console.log(`${new Date()}  App Started. Listening on ${host}:${port}, serving ${clientApp}`);
 });
 
+app.get('/chat', (req, res) => {
+	let chatDetails = chatrooms.map(room => {
+	  return {
+		id: room.id,
+		name: room.name,
+		image: room.image,
+		messages: messages[room.id] // attach messages corresponding to the room id
+	  };
+	});
+	res.json(chatDetails);
+  });
+  
+
 cpen322.connect('http://3.98.223.41/cpen322/test-a3-server.js');
-cpen322.export(__filename, { app });
+cpen322.export(__filename, { app, chatrooms, messages });
