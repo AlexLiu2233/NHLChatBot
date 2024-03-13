e2141028b0ae94389ec449c6d5604b9951fe19c5
AlexLiu2233 <alexliu4043@gmail.com>
2024-03-12 23:01:34 -0700

    Task 2 Done

--- Database.js
+++ Database.js
@@ -100,6 +100,19 @@ Database.prototype.addConversation = function (conversation) {
     );
 };
 
+// Add this method to the Database class in your Database.js file
+
+Database.prototype.getUser = async function(username) {
+    const db = await this.connected;
+    try {
+        const user = await db.collection('users').findOne({ username: username });
+        if (!user) return null;
+        return user; // or return { ...user, _id: user._id.toString() }; if you want to convert ObjectId to string
+    } catch (error) {
+        console.error("Error fetching user from MongoDB:", error);
+        throw error;
+    }
+}
 
 
 module.exports = Database;
